1d35effca496ebb106df7c3c25933dcf17f23af7
JunkuiZhang <364772080@qq.com>
2016-02-29 15:25:56 +0800

    V0.8 error controler added

--- get_course_id.py
+++ get_course_id.py
@@ -9,45 +9,91 @@ class Course:
 		self.user = user
 		self.pwd = pwd
 
+	def connectServer(self, url, response, headers):
+		try:
+			res = response.get(url, headers=headers)
+		except:
+			print("Cant connect to the server.")
+			raise ConnectionError
+		if re.findall("游客登录", res.text) != []:
+			print("="*6 + "Invalid captcha or information." + "="*6)
+			raise ValueError
+		elif re.findall("404.png", res.text) != []:
+			print("="*6 + "Server error." + "="*6)
+			raise ValueError
+		else:
+			return {"res": res, "session": response}
+
+	def connectServerPost(self, url, response, headers, data):
+		try:
+			res = response.post(url, headers=headers, data=data)
+		except:
+			print("Cant connect to the server.")
+			raise ConnectionError
+		if re.findall("游客登录", res.text) != []:
+			print("="*6 + "Invalid captcha or information." + "="*6)
+			raise ValueError
+		elif re.findall("404.png", res.text) != []:
+			print("="*6 + "Post server error." + "="*6)
+			raise ConnectionError
+		else:
+			return response
+
 	def getCaptha(self):
 		url = "http://210.42.121.241/servlet/GenImg"
 		response = requests.session()
-		res = response.get(url)
-		f = open("0.jpg", "wb")
-		f.write(res.content)
-		f.close()
+		conn = self.connectServer(url, response, headers={})
+		res = conn["res"]
+		try:
+			f = open("0.jpg", "wb")
+			f.write(res.content)
+			f.close()
+		except ValueError("Can't write the captcha picture."):
+			raise
 		captcha = input("Enter the strings: ")
 		cookie = res.cookies
 		cookie = re.findall('kie (.*) for', str(cookie))[0]
-		return [captcha, response, cookie]
+		return {"captcha":captcha, "session": conn["session"], "cookie": cookie}
 
-	def main(self, ls):
-		captcha = ls[0]
-		response = ls[1]
-		cookie = ls[2]
-		login_url = "http://210.42.121.241/servlet/Login"
+	def checkUser(self, response, cookie, captcha):
 		headers = {
 			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0",
 			"Cookie": cookie
 		}
+		login_url = "http://210.42.121.241/servlet/Login"
 		post_data = {
 			"id": self.user,
 			"pwd": self.pwd,
 			"xdvfb": captcha
 		}
-		res0 = response.post(login_url, headers=headers, data=post_data)
+		response = self.connectServerPost(login_url, response, headers=headers, data=post_data)
 		list_url = "http://210.42.121.241/stu/choose_PubLsn_list.jsp?XiaoQu=0&credit=0&keyword=&pageNum=1"
-		res1 = response.get(list_url, headers=headers)
-		soup = bs4.BeautifulSoup(res1.text, "html.parser")
+		conn = self.connectServer(list_url, response=response, headers=headers)
+		return conn
+
+	def main(self, ls):
+		captcha = ls["captcha"]
+		response = ls["session"]
+		cookie = ls["cookie"]
+		res = self.checkUser(response, cookie, captcha)
+		soup = bs4.BeautifulSoup(res["res"].text, "html.parser")
 		pagenum = soup.find_all("div")[-1]
 		pagenum = re.findall('记录 第1/([0-9]+)页', str(pagenum))[0]
-		f = open("course_info.csv", "w")
-		w = csv.writer(f)
-		w.writerow(["CourseName", "Credit", "Class"])
+		try:
+			f = open("course_info.csv", "w", newline="")
+			w = csv.writer(f)
+			w.writerow(["CourseName", "Credit", "Class", "CourseID"])
+		except ValueError("Cant write information in file!"):
+			raise
+		headers = {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0",
+			"Cookie": cookie
+		}
+		response = res["session"]
 		for i in range(int(pagenum)):
 			list_url = "http://210.42.121.241/stu/choose_PubLsn_list.jsp?XiaoQu=0&credit=0&keyword=&pageNum=" + str(i + 1)
-			res2 = response.get(list_url, headers=headers)
-			soup = bs4.BeautifulSoup(res2.text, "html.parser")
+			res2 = self.connectServer(list_url, response=response, headers=headers)
+			soup = bs4.BeautifulSoup(res2["res"].text, "html.parser")
 			course = soup.find_all("tr")
 			course.pop(0)
 			for c in course:
@@ -57,13 +103,17 @@ class Course:
 				name = re.findall('<td>(.*)<', str(info[0]))[0]
 				credit = re.findall('<td>(.*)<', str(info[1]))[0]
 				_class = str(s.find_all("div")[-1].string).strip()
+				ids = info[-1]
+				c_id = re.findall('" id="([0-9]+)" ', str(ids))[0]
 				print(name)
 				print(credit)
 				print(_class)
-				w.writerow([name, credit, _class])
+				print(c_id)
+				w.writerow([name, credit, _class, c_id])
 				print("Done.")
 		f.close()
 
-c = Course()
-ls = c.getCaptha()
-c.main(ls)
+if __name__ == "__main__":
+	c = Course()
+	ls = c.getCaptha()
+	c.main(ls)
