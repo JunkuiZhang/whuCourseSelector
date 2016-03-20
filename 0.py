56126d33b8bfb3a428259767cbf1819ba370a018
JunkuiZhang <364772080@qq.com>
2016-03-20 23:25:55 +0800

    dont know why

--- 0.py
+++ 0.py
@@ -3,23 +3,29 @@ import re
 from ui_test import Ui_MainWindow
 from PyQt4 import QtGui, QtCore
 import sys
+import threading
 
 
 class ConnectThread(QtCore.QThread):
 	sin1 = QtCore.pyqtSignal(str)
 
 	def __init__(self, response, url):
+		# threading.Thread.__init__(self)
 		QtCore.QThread.__init__(self)
 		self.response = response
 		self.url = url
+		self.strings = "gdfgd"
 
 	def run(self):
-		try:
-			self.sin1.emit("???")
-			self.response.get(self.url)
-			self.sin1.emit("Yes")
-		except:
-			self.sin1.emit("No")
+		while True:
+			self.sin1.emit("Trying")
+			try:
+				self.response.get(self.url)
+				self.sin1.emit("Hello")
+				break
+			except:
+				self.sin1.emit("No")
+				continue
 
 
 class MyWindow(QtGui.QMainWindow, Ui_MainWindow):
@@ -35,14 +41,23 @@ class MyWindow(QtGui.QMainWindow, Ui_MainWindow):
 		self.get_captcha.clicked.connect(self.connect_to_captcha)
 		self.response = requests.session()
 
-	def print_something(self, string):
-		print(string)
+	def update_text(self, string):
+		self.print_window.append(string)
 
 	def connect_to_captcha(self):
-		url = "https://www.google.com"
+		url = "http://210.42.121.241/servlet/GenImg"
+		url = "www.google.com"
+		# thread = ConnectToServer(self.response, url)
+		# thread.start()
+		# print(thread.strings)
 		thread = ConnectThread(self.response, url)
-		thread.sin1.connect(self.print_something)
-		thread.moveToThread(self.response, url)
+		thread.sin1.connect(self.update_text)
+		thread.start()
+		# th = LoopConnect(thread)
+		# th.sin1.connect(self.let_do_something)
+		# th.start()
+		thread.wait()
+		# th.wait()
 
 
 if __name__ == "__main__":
